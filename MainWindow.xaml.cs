da1282f40035faf7bef38683884fcd6925f6c92c
Roxi <ganieva0114@gmail.com>
2023-11-06 16:21:13 +0500

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CurrentMatches
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Match> matches = new List<Match>();
+            matches.Add(new Match() { Team1 = "Bayern Munich", Team2 = "Real Madrid", Score1 = 3, Score2 = 2, Completion = 87 });
+            matches.Add(new Match() { Team1 = "Verona", Team2 = "Monza", Score1 = 1, Score2 = 3, Completion = 78 });
+            matches.Add(new Match() { Team1 = "Arsenal T", Team2 = "Family", Score1 = 0, Score2 = 0, Completion = 35 });
+            matches.Add(new Match() { Team1 = "Parma", Team2 = "South Tylor", Score1 = 1, Score2 = 0, Completion = 46 });
+            matches.Add(new Match() { Team1 = "Hacken", Team2 = "Malmo", Score1 = 3, Score2 = 1, Completion = 74 });
+
+            lbMatches.ItemsSource = matches;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbMatches.SelectedItem != null)
+            {
+                MessageBox.Show("Selected match: " +
+                    (lbMatches.SelectedItem as Match).Team1 + " " +
+                    (lbMatches.SelectedItem as Match).Score1 + " " +
+                    (lbMatches.SelectedItem as Match).Score2 + " " +
+                    (lbMatches.SelectedItem as Match).Team2);
+            }
+        }
+    }
+
+
+    public class Match
+    {
+        public int Score1 { get; set; }
+        public int Score2 { get; set; }
+        public string Team1 { get; set; }
+        public string Team2 { get; set; }
+        public int Completion { get; set; }
+
+
+    }
+}
